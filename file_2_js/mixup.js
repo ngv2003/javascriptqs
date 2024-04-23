85b7dd6eaa86c83c143086ffe2dbbcd38f5b49cb
ngv2003 <138695421+ngv2003@users.noreply.github.com>
2024-04-23 08:08:50 +0530

    mixup done

--- file_2_js/mixup.js
+++ file_2_js/mixup.js
@@ -1,1 +1,8 @@
-fi
+function mixup(str1,str2)
+{
+    finalstr1=str2.slice(0,2)+str1.slice(2);
+    finalstr2=str1.slice(0,2)+str2.slice(2);
+
+    console.log(finalstr1+" "+finalstr2);
+}
+mixup('mix','pod');
